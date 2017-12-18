85c7445ba62f06768c6e18bd9ca66e490d3bdc1d
Mriganga 10 <mriganga.deka1996@gmail.com>
2017-12-18 13:53:10 -0800

    do

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+//var bodyParser = require('body-parser');
+//var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+//var {Todo} = require('./models/todo');
+//var {User} = require('./models/user');
 
 var app = express();
 
