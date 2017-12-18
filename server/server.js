0088084398bd1b3164b469fb56dc5618e5febe1f
Mriganga 10 <mriganga.deka1996@gmail.com>
2017-12-18 14:57:07 -0800

    do

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
-//var {mongoose} = require('./db/mongoose.js');
+var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
-/*
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req,res) => {
@@ -50,7 +50,7 @@ app.get('/todos/:id', (req,res) => {
 		});
 	}
 });
-*/
+
 app.listen(port, () => {
 	console.log(`Server is up on port ${port}`);
 });
