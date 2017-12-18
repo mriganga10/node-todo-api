b40309f9499a3454fb28be091222a223be1f68c8
Mriganga 10 <mriganga.deka1996@gmail.com>
2017-12-18 13:41:30 -0800

    dont

--- server/server.js
+++ server/server.js
@@ -1,14 +1,56 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+/*var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
+*/
 var app = express();
 
 var port = process.env.PORT || 3000;
+/*
+app.use(bodyParser.json());
 
+app.post('/todos', (req,res) => {
+	var todo = new Todo({
+		text: req.body.text
+	});
+
+	todo.save().then((doc) => {
+		res.send(doc);
+	}, (e) => {
+		res.status(400).send(e);
+	});
+});
+
+app.get('/todos',(req,res) => {
+	Todo.find().then((todos) => {
+		res.send({todos});
+	}, (e) => {
+		res.status(400).send(e);
+	});
+});
+
+//GET /todos/1234324
+app.get('/todos/:id', (req,res) => {
+	var id = req.params.id;
+	if(!ObjectID.isValid(id)){
+		res.status(404).send();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+	}
+	else{
+		Todo.findById(id).then((todo) => {
+			if(!todo){
+				res.status(404).send();
+			}
+			else{
+				res.send(todo);
+			}
+		}).catch((err) => {
+			res.catch(404).send();
+		});
+	}
+});
+*/
 app.listen(port, () => {
 	console.log(`Server is up on port ${port}`);
 });
