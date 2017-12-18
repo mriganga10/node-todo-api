ccf402155f0e3be90758cbe1fd264e0300362341
Mriganga 10 <mriganga.deka1996@gmail.com>
2017-12-18 10:08:25 -0800

    GET todo/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -27,6 +27,26 @@ app.get('/todos',(req,res) => {
 	}, (e) => {
 		res.status(400).send(e);
 	});
+});
+
+//GET /todos/1234324
+app.get('/todos/:id', (req,res) => {
+	var id = req.params.id;
+	if(!ObjectID.isValid(id)){
+		res.status(404).send();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+	}
+	else{
+		Todo.findById(id).then((todo) => {
+			if(!todo){
+				res.status(404).send();
+			}
+			else{
+				res.send(todo);
+			}
+		}).catch((err) => {
+			res.catch(404).send();
+		});
+	}
 });
 
 app.listen(3000, () => {
