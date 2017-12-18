8a4539b584b7272b78abfe0bdbd3a0cdd6655237
Mriganga 10 <mriganga.deka1996@gmail.com>
2017-12-18 10:39:11 -0800

    test GET todo/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,12 +1,14 @@
 const expect = require('expect');
 const request = require('supertest');
-
+const {ObjectID} = require('mongodb');
 const {app} = require('./../server.js');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+	_id : new ObjectID(),
 	text: 'have lunch'
 },{
+	_id : new ObjectID(),
 	text: 'have breakfast'
 }];
 
@@ -66,4 +68,23 @@ describe('POST /todos', () => {
 			})
 			.end(done);
 		})
+	});
+
+	describe('GET /todos/:id', () => {
+		it('should return todo doc', (done) => {
+				request(app)
+				.get(`/todos/${todos[0]._id.toHexString()}`)
+				.expect(200)
+				.expect((res) => {
+					expect(res.body.text).toBe(todos[0].text);
+				})
+				.end(done);
+		});
+		it('should return 404 if todo not found', (done) => {
+			request(app)
+			.get(`/todos/5a38092d05035a1f08d98a81`)
+			.expect(404)
+			.end(done);
+		});
+		
 	});
