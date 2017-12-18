cadbaafeb6770c5ee8ee94e9aee0ad14fd68eafe
Mriganga 10 <mriganga.deka1996@gmail.com>
2017-12-18 07:27:00 -0800

    GET todo test

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,12 +4,20 @@ const request = require('supertest');
 const {app} = require('./../server.js');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+	text: 'have lunch'
+},{
+	text: 'have breakfast'
+}];
+
 beforeEach((done) => {
-	Todo.remove({}).then(() => done());
+	Todo.remove({}).then(() => {
+		return Todo.insertMany(todos);
+	}).then(() => done());
 });
 
 describe('POST /todos', () => {
-	/*it('should crete new todo', (done) => {
+	it('should crete new todo', (done) => {
 		var text = 'Test todo test';
 
 		request(app)
@@ -23,13 +31,13 @@ describe('POST /todos', () => {
 			if(err){
 				return done(err);
 			}
-			Todo.find().then((todos) => {
+			Todo.find({text}).then((todos) => {
 				expect(todos.length).toBe(1);
 				expect(todos[0].text).toBe(text);
 				done();
 			}).catch((e)  => done(e));
 		});
-	});*/
+	});
 	//todo does not get created when sent bad data
 	it('should not create new todo', (done) => {
 		request(app).
@@ -41,9 +49,21 @@ describe('POST /todos', () => {
 				return done(err);
 			}
 			Todo.find().then((todos) => {
-				expect(todos.length).toBe(0);
+				expect(todos.length).toBe(2);
 				done();
 			}).catch((e) => done(e));
 		});
 	});
 });
+
+	describe('GET /todos',() => {
+		it('should get todos',(done) => {
+			request(app)
+			.get('/todos')
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todos.length).toBe(2);
+			})
+			.end(done);
+		})
+	});
