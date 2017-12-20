c85e40ba3c955ecd84e4722ce34b942d2f0e9e3c
Mriganga 10 <mriganga.deka1996@gmail.com>
2017-12-20 03:23:04 -0800

    authentication initial commit

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 var _ = require('lodash');
+var {authenticate} = require('./middleware/authenticate');
 var app = express();
 
 var port = process.env.PORT || 3000;
@@ -101,6 +102,23 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send();
   })
+});
+
+app.post('/user',(req,res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
+app.get('/user/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
--- server/test/server.test.js
+++ server/test/server.test.js
@@ -9,7 +9,9 @@ const todos = [{
 	text: 'have lunch'
 },{
 	_id : new ObjectID(),
-	text: 'have breakfast'
+	text: 'have breakfast',
+	 completed: true,
+  completedAt: 333
 }];
 
 beforeEach((done) => {
@@ -110,3 +112,42 @@ describe('POST /todos', () => {
 			});
 		});
 	});
+	describe('PATCH /todos/:id', () => {
+  it('should update the todo', (done) => {
+    var hexId = todos[0]._id.toHexString();
+    var text = 'This should be the new text';
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        completed: true,
+        text
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end(done);
+  });
+
+  it('should clear completedAt when todo is not completed', (done) => {
+    var hexId = todos[1]._id.toHexString();
+    var text = 'This should be the new text!!';
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        completed: false,
+        text
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.completedAt).toNotExist();
+      })
+      .end(done);
+  });
+});
